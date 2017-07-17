7ba48c091ec4ef128f9ebb9b5def2a7e0811b485
Tony Weng <tcweng91@gmail.com>
2017-07-16 23:38:53 -0700

    remove excess css

--- assets/js/gif.js
+++
@@ -1,7 +0,0 @@
-$(document).read(function(){
-  $(".hover2").mouseover(function () {
-    $(this).attr('src', $(this).data("hover"));
-  }).mouseout(function () {
-    $(this).attr('src', $(this).data("src"));
-  });
-});
